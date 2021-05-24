878e55c1334f8f1d747e9e0bffddf314cedbd98a
epsilon-deltta <kokomong1316@gmail.com>
2021-05-24 17:42:23 +0900

    relative path for general usage, not only colab😅😅

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,6 +1,8 @@
 from utils import create_data_lists
-
+import os
 if __name__ == '__main__':
-    create_data_lists(voc07_path='/content/VOCdevkit/VOC2007',
-                      voc12_path='/content/VOCdevkit/VOC2012',
+    voc07 = os.path.abspath('./VOCdevkit/VOC2007')
+    voc12 = os.path.abspath('./VOCdevkit/VOC2012')
+    create_data_lists(voc07_path=voc07,
+                      voc12_path=voc12,
                       output_folder='./')
